dcc17f5284b717ea2f79723e21c9762d0c574f7c
Og Astorga <og@ogastorga.com>
2018-07-17 18:48:31 -0500

    Parse incoming JSON bodies

--- app/controllers/telegram.rb
+++ app/controllers/telegram.rb
@@ -1,10 +1,31 @@
 require_relative './app'
 
 class TelegramController < AppController
+  before do
+    request.body.rewind
+    body = request.body.read
+
+    @body_hash = Hash.new
+    if body.size > 0
+      begin
+        hash = JSON.parse body
+        @body_hash = hash
+      rescue
+        # @TODO log to stderr
+      end
+    end
+  end
+
   post '/webhook/:secret' do
     if params[:secret] != ENV['TELEGRAM_SECRET']
       halt 401
     end
+
+    if @body_hash.has_key?('update_id') == false
+      halt 400
+    end
+
+    puts JSON.pretty_generate(@body_hash)
 
     [200, 'grant']
   end
--- app/controllers/telegram_spec.rb
+++ app/controllers/telegram_spec.rb
@@ -11,10 +11,25 @@ class TelegramControllerSpec < MiniTest::Test
   end
 
   def test_webhook_auth
-    post '/webhook/r8O3JnU3O91uegYj'
+    post '/webhook/r8O3JnU3O91uegYj', '{"update_id": 1}'
     assert_equal last_response.status, 401
 
-    post "/webhook/#{ENV['TELEGRAM_SECRET']}"
+    post "/webhook/#{ENV['TELEGRAM_SECRET']}", '{"update_id": 1}'
+    assert_equal last_response.status, 200
+  end
+
+  def test_chat_malformed
+    body = '{"updated_id": 1'
+    post "/webhook/#{ENV['TELEGRAM_SECRET']}", body
+
+    assert_equal last_response.status, 400
+  end
+
+  def test_chat_init
+    body = '{"update_id":1,
+    "message":{"message_id":1,"from":{"id":1,"is_bot":false,"first_name":"Milan","last_name":"Kundera","username":"mkundera","language_code":"en-us"},"chat":{"id":1,"first_name":"Milan","last_name":"Kundera","username":"mkundera","type":"private"},"date":1531867796,"text":"/start","entities":[{"offset":0,"length":6,"type":"bot_command"}]}}'
+    post "/webhook/#{ENV['TELEGRAM_SECRET']}", body
+
     assert_equal last_response.status, 200
   end
 end
