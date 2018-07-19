3c8cb26dc57bc4b2c5a521f776db92484f722f46
Og Astorga <og@ogastorga.com>
2018-07-19 11:02:23 -0500

    use mongoid

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -1,12 +1,33 @@
 require 'sinatra/base'
+require 'mongoid'
 
 class AppController < Sinatra::Base
   configure :production, :development do
     enable :logging
+
+    Mongoid.logger.level = Logger::WARN
+    Mongo::Logger.logger.level = Logger::WARN
+
+    Mongoid.configure do |config|
+      config.clients.default = {
+        hosts: [ENV['MONGODB_HOST']],
+        database: ENV['MONGODB_DB'],
+      }
+    end
   end
 
   configure :test do
     set :raise_errors, true
     set :show_exceptions, false
+
+    Mongoid.logger.level = Logger::WARN
+    Mongo::Logger.logger.level = Logger::WARN
+
+    Mongoid.configure do |config|
+      config.clients.default = {
+        hosts: [ENV['MONGODB_HOST']],
+        database: ENV['MONGODB_TESTDB'],
+      }
+    end
   end
 end
