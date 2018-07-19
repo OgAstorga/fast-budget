83877c5d98aba1409b4607630f194f8e37b4ab17
Og Astorga <og@ogastorga.com>
2018-07-19 18:19:00 -0500

    update_generator helper to make test easier

--- app/controllers/telegram_spec.rb
+++ app/controllers/telegram_spec.rb
@@ -4,6 +4,39 @@ require 'rack/test'
 require_relative './telegram.rb'
 require_relative '../models/user.rb'
 
+def update_generator(user, message)
+  update = {
+    'update_id' => 1,
+    'message' => {
+      'message_id' => message['id'],
+      'from' => {
+        'id' => user['id'],
+        'is_bot' => false,
+        'first_name' => user['first_name'],
+        'last_name' => user['last_name'],
+        'username' => user['username'],
+        'language_code' => 'en-us',
+      },
+      'chat' => {
+        'id' => user['id'],
+        'first_name' => user['first_name'],
+        'last_name' => user['last_name'],
+        'username' => user['username'],
+        'type' => 'private',
+      },
+      'date' => Time.now.to_i,
+      'text' => message['text'],
+      'entities' => [{
+        'offset' => 0,
+        'length' => message['text'].length,
+        'type' => 'bot_command',
+      }],
+    },
+  }
+
+  update.to_json
+end
+
 class TelegramControllerSpec < MiniTest::Test
   include Rack::Test::Methods
 
@@ -20,16 +53,21 @@ class TelegramControllerSpec < MiniTest::Test
   end
 
   def test_chat_malformed
-    body = '{"updated_id": 1'
-    post "/webhook/#{ENV['TELEGRAM_SECRET']}", body
+    post "/webhook/#{ENV['TELEGRAM_SECRET']}", '{"updated_id": 1'
 
     assert_equal last_response.status, 400
   end
 
   def test_chat_init
-    body = '{"update_id":1,
-    "message":{"message_id":1,"from":{"id":1,"is_bot":false,"first_name":"Milan","last_name":"Kundera","username":"mkundera","language_code":"en-us"},"chat":{"id":1,"first_name":"Milan","last_name":"Kundera","username":"mkundera","type":"private"},"date":1531867796,"text":"/start","entities":[{"offset":0,"length":6,"type":"bot_command"}]}}'
-    post "/webhook/#{ENV['TELEGRAM_SECRET']}", body
+    post "/webhook/#{ENV['TELEGRAM_SECRET']}", update_generator({
+      'id' => 1,
+      'username' => 'mkundera',
+      'first_name' => 'Milan',
+      'last_name' => 'Kundera',
+    }, {
+      'id' => 1,
+      'text' => '/start',
+    })
 
     assert User.where(_id: 1).exists?
 
