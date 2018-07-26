6bbf72b8f3f27c4fa66cfb5eef88eda8158cf59c
Og Astorga <og@ogastorga.com>
2018-07-26 12:40:49 -0500

    helper to make requests to the telegram bot api

---
+++ app/helpers/bot-api.rb
@@ -0,0 +1,53 @@
+require 'rest-client'
+
+require_relative './mock-client'
+
+class BotApi
+  class << self
+    def build_uri(method_name)
+      uri = String.new ENV['TELEGRAM_URI']
+      uri = uri.sub! '<TELEGRAM_TOKEN>', ENV['TELEGRAM_TOKEN']
+      uri = uri.sub! '<METHOD_NAME>', method_name
+    end
+
+    def send_message(params)
+      # build telegram uri
+      uri = build_uri 'sendMessage'
+
+      required = [
+        :chat_id,
+        :text
+      ]
+      optional = [
+        :parse_mode,
+        :disable_web_page_preview,
+        :disable_notification,
+        :reply_to_message_id,
+        :reply_markup
+      ]
+
+      # build request payload
+      data = Hash.new
+      required.each do |attr|
+        data[attr] = params[attr]
+      end
+      optional.each do |attr|
+        data[attr] = params[attr] if !params[attr].nil?
+      end
+      payload = data.to_json
+
+      # build headers
+      headers = { content_type: :json, accept: :json }
+
+      if ENV['RACK_ENV'] == 'test'
+        MockClient.post uri, payload, headers
+      else
+        begin
+          RestClient.post uri, payload, headers
+        rescue RestClient::ExceptionWithResponse => err
+          # @TODO Log failed request
+        end
+      end
+    end
+  end
+end
---
+++ app/helpers/bot-api_spec.rb
@@ -0,0 +1,25 @@
+require 'minitest/autorun'
+require 'rack/test'
+
+require_relative './mock-client'
+require_relative './bot-api'
+
+class TelegramControllerSpec < MiniTest::Test
+  include Rack::Test::Methods
+
+  def setup
+    MockClient.requests = []
+  end
+
+  def test_send_message
+    BotApi.send_message chat_id: 1, text: 'chat'
+
+    url = String.new ENV['TELEGRAM_URI']
+    url = url.sub! '<TELEGRAM_TOKEN>', ENV['TELEGRAM_TOKEN']
+    url = url.sub! '<METHOD_NAME>', 'sendMessage'
+
+    assert_equal 1, MockClient::requests.length
+    assert_equal url, MockClient::requests[0][:url]
+    assert_equal '{"chat_id":1,"text":"chat"}', MockClient::requests[0][:payload]
+  end
+end
